51f11ecb6dd168268d64b0b13ea143eee8b3ccb5
sledziu87 <sledzinski.bartosz@gmail.com>
2018-01-28 14:23:26 +0100

    prepare for define a InputComp state

--- src/InputComponent.js
+++ src/InputComponent.js
@@ -4,13 +4,23 @@ import './InputComponent.css';
 
 class InputComp extends React.Component{
 
+//  constructor(props){
+//      super (props)
+//  }
+//
+
     render () {
         return (
           <div>
               <form>
                   <input type="text" placeholder="type NIP" name="nip" />
+                  {/*put in here onChande and value for state */}
+
                   <input type="button" value="SEND REQUEST"/>
+                  {/*need some handleClick*/}
               </form>
+
+              {/*there need to be a field for inported info - maybe a modal ? :) */}
           </div>
 
         );
