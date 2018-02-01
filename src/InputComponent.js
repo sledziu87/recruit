69b3405e4420272562d23d838d923d1a0ff34bd0
sledziu87 <sledzinski.bartosz@gmail.com>
2018-02-01 22:30:59 +0100

    input: it works on test REST GET

--- src/InputComponent.js
+++ src/InputComponent.js
@@ -5,27 +5,43 @@ import './InputComponent.css';
 class InputComp extends React.Component {
 
     constructor(props){
-     super (props);
+        super (props);
         this.state = {
+            data: [],
             form: {
                 nip: ''
             }
+        };
+
+        // this.onChange = this.onChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
         }
-    }
+
+        handleClick(event){
+        event.preventDefault();
+        const dataLoader = new XMLHttpRequest();
+        dataLoader.open('GET', "http://jsonplaceholder.typicode.com/users?phone="+"024-648-3804" ); //024-648-3804 dodac zmienna z onchange todo change request to defined by task !
+            //todo Build and use value frome input in exch of typed phone nr
+            dataLoader.onload = () => {
+                this.setState({data: dataLoader.responseText});
+            };
+            dataLoader.send(null);
+        }
 
 
 
     render () {
+        const { data } = this.state;
         return (
           <div>
               <form>
                   <input type="text" placeholder="type NIP" name="nip" required="required" onChange={this.onChange} value={this.state.nip}/>
+                  {/* At this place is required info for specific way to type NIP  */}
 
-                  <input type="button" value="SEND REQUEST"/>
-                  {/*need some handleClick*/}
+                  <input type="button" value="SEND REQUEST" onClick={this.handleClick}/> {/* handleClick*/}
               </form>
-
-              {/* place for imported info.  */}
+              <textarea cols="50" rows="30" value={data}/> {/* place for imported info.  */}
               {/* NEXT COMPO ? Modal for info from history */}
           </div>
 
