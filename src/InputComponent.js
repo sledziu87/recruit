599ca803eb6f4913c9fa108d09266c840f121605
sledziu87 <sledzinski.bartosz@gmail.com>
2018-02-01 10:25:27 +0100

    input build state

--- src/InputComponent.js
+++ src/InputComponent.js
@@ -2,25 +2,31 @@ import React from 'react';
 import './InputComponent.css';
 
 
-class InputComp extends React.Component{
+class InputComp extends React.Component {
 
-//  constructor(props){
-//      super (props)
-//  }
-//
+    constructor(props){
+     super (props);
+        this.state = {
+            form: {
+                nip: ''
+            }
+        }
+    }
+
+
 
     render () {
         return (
           <div>
               <form>
-                  <input type="text" placeholder="type NIP" name="nip" />
-                  {/*put in here onChande and value for state */}
+                  <input type="text" placeholder="type NIP" name="nip" required="required" onChange={this.onChange} value={this.state.nip}/>
 
                   <input type="button" value="SEND REQUEST"/>
                   {/*need some handleClick*/}
               </form>
 
-              {/*there need to be a field for inported info - maybe a modal ? :) */}
+              {/* place for imported info.  */}
+              {/* NEXT COMPO ? Modal for info from history */}
           </div>
 
         );
